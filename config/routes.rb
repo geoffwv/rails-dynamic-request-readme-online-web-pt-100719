bea8cd8c3ca9df88d9be58cc35be0cec4860f9c1
github username <github email address>
2020-02-18 18:47:52 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get 'posts/:id', to: 'posts#show' this was removed and the end of the lesson and replaced with... 
+  resources :posts, only: :show
 end
